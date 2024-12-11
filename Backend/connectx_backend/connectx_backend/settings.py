259470539143a02f760086c8bd01b32bd2320fe1
Adane Moges <adman19940805@gmail.com>
2024-12-12 00:23:56 +0300

    Update authentication settings to use session authentication and impliment stock-request endpoint

--- Backend/connectx_backend/connectx_backend/settings.py
+++ Backend/connectx_backend/connectx_backend/settings.py
@@ -130,8 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
