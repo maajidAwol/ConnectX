0fb70b9ac0eefa195eeb145f2314efb0dc972002
abel <abelaatkelet@gmail.com>
2024-12-18 06:11:50 +0300

    create-end-points

--- Backend/connectx_backend/products/serializers.py
+++ Backend/connectx_backend/products/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Product
 
-class UserSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
