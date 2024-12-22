c5516c67b7a26b978145a2587ad3314f05d2f196
Adane Moges <adman19940805@gmail.com>
2024-12-23 00:46:32 +0300

    Add Swagger UI endpoint for easier API documentation access

--- Backend/connectx_backend/connectx_backend/urls.py
+++ Backend/connectx_backend/connectx_backend/urls.py
@@ -39,6 +39,6 @@ urlpatterns = [
     path('api/', include('users.urls')),
     path('api/', include('stock_requests.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
