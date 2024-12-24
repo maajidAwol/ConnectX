39e29a9f0800f55cf91b24ca71383d05467ef79a
abel <abelaatkelet@gmail.com>
2024-12-24 21:21:07 +0300

    migrate

--- Backend/connectx_backend/products/models.py
+++ Backend/connectx_backend/products/models.py
@@ -5,11 +5,11 @@ from django.conf import settings
 # Category Model
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(default='No description',blank=True)
+    description = models.TextField(default='No description', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-
 
 # Product Model
 class Product(models.Model):
@@ -17,15 +17,16 @@ class Product(models.Model):
         primary_key=True, default=uuid.uuid4, editable=False
     )
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Reference the Category model
+    description = models.TextField(default='No description')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField()
     shared_inventory = models.BooleanField(default=False)
     seller = models.ForeignKey(
-        settings.AUTH_USER_MODEL,  # Use the custom user model
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='products'
+        related_name='products',
+        default=1  # Ensure user with ID 1 exists
     )
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
