d27749b7773bc9b697c1e5787aae61fc235487ae
Adane Moges <adman19940805@gmail.com>
2024-12-27 09:55:28 +0300

    Add total_payment field to StockRequest model and calculate it on save

--- Backend/connectx_backend/stock_requests/models.py
+++ Backend/connectx_backend/stock_requests/models.py
@@ -6,6 +6,10 @@ class StockRequest(models.Model):
     entrepreneur = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'entrepreneur'})
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     requested_quantity = models.IntegerField()
+    total_payment = models.DecimalField(max_digits=10, decimal_places=2)
+    def save(self, *args, **kwargs):
+        self.total_payment = self.product.price * self.requested_quantity
+        super().save(*args, **kwargs)
     status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Approved', 'Approved'), ('Rejected', 'Rejected')], default='Pending')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
