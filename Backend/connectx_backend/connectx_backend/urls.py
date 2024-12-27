21ed1443ddeaf368965e16867985d504c46f31c1
abel <abelaatkelet@gmail.com>
2024-12-27 14:24:04 +0300

    url-add-transaction

--- Backend/connectx_backend/connectx_backend/urls.py
+++ Backend/connectx_backend/connectx_backend/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
     path('api/', include('stock_requests.urls')),
     path('api/', include('products.urls')),
     path('api/', include('orders.urls')),
+    path('api/', include('transactions.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
