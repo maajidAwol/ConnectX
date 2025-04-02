c2bbc26fce3c2758bc2e16af4f6c7d7bf8048eb7
Adane Moges <adanemoges6@gmail.com>
2025-04-02 06:06:02 +0300

    Remove unused migration files and database

--- Backend/connectx_backend/products/models.py
+++ Backend/connectx_backend/products/models.py
@@ -10,14 +10,14 @@ class Product(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_products")
     sku = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=255)
-    base_price = models.DecimalField(max_digits=8, decimal_places=2)
+    base_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
     profit_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
-    selling_price = models.DecimalField(max_digits=8, decimal_places=2)
-    quantity = models.IntegerField()
+    selling_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    quantity = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     is_public = models.BooleanField(default=False)
-    description = models.TextField()
-    cover_url = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default="")
+    cover_url = models.CharField(max_length=255, blank=True, default="")
     images = models.JSONField(default=list, blank=True)  # List of image URLs
     colors = models.JSONField(default=list, blank=True)  # List of colors
     sizes = models.JSONField(default=list, blank=True)  # List of sizes
