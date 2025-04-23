185e830ba528ca23e6a2faf2c27bb2cdc6d0f307
Adane Moges <adanemoges6@gmail.com>
2025-04-23 09:38:58 +0300

    Update database settings to use PostgreSQL with environment variables for configuration

--- Backend/connectx_backend/connectx_backend/settings.py
+++ Backend/connectx_backend/connectx_backend/settings.py
@@ -97,13 +97,20 @@ WSGI_APPLICATION = "connectx_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
