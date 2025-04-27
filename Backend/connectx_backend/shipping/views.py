cb0bdc6439ff02221bdb92f700171afe24509f92
Adane Moges <adanemoges6@gmail.com>
2025-04-27 22:39:43 +0300

    refactor: remove IsTenantAdmin permission from OrderShippingAddressViewSet

--- Backend/connectx_backend/shipping/views.py
+++ Backend/connectx_backend/shipping/views.py
@@ -1,11 +1,10 @@
 from rest_framework import viewsets, permissions
 from .models import OrderShippingAddress
 from .serializers import OrderShippingAddressSerializer
-from users.permissions import IsTenantAdmin
 
 class OrderShippingAddressViewSet(viewsets.ModelViewSet):
     serializer_class = OrderShippingAddressSerializer
-    permission_classes = [permissions.IsAuthenticated, IsTenantAdmin]
+    permission_classes = [permissions.IsAuthenticated,]
 
     def get_queryset(self):
         """Ensure tenants can only access their own orders' shipping addresses."""
