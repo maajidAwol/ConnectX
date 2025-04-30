b18788cc6415fc192103d14ce884a4681c3561b9
Adane Moges <adanemoges6@gmail.com>
2025-04-30 21:23:17 +0300

    test: add user creation tests for tenant model

--- Backend/connectx_backend/tenants/tests.py
+++ Backend/connectx_backend/tenants/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from tenants.models import Tenant
 
-# Create your tests here.
+
+class TenantUserSignalTest(TestCase):
+    def test_user_created_on_tenant_creation(self):
+        tenant_email = "tenantuser@example.com"
+        tenant = Tenant.objects.create(
+            name="TenantWithUser", email=tenant_email, password="tenantpass123"
+        )
+        User = get_user_model()
+        user = User.objects.filter(email=tenant_email, tenant=tenant).first()
+        self.assertIsNotNone(user)
+        self.assertEqual(user.email, tenant_email)
+        self.assertEqual(user.tenant, tenant)
