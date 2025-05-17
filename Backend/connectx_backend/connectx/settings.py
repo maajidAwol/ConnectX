d6d0759febbef70fa1dd565bd5ac2e1fa7c1ed3f
Adane Moges <adanemoges6@gmail.com>
2025-05-17 09:43:15 +0300

    fix: update static file settings for correct directory structure

--- Backend/connectx_backend/connectx/settings.py
+++ Backend/connectx_backend/connectx/settings.py
@@ -33,8 +33,11 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
+# Application definition
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # dev static files
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")    # where collectstatic puts files
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
